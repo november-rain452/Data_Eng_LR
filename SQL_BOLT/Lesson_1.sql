93892975b93924f4d9b7f76b32e2795c9d55844c
Aditya Sinha <ranjanaditya452@gmail.com>
2026-08-20 23:46:12 +0530

    Lesson 1

---
+++ SQL_BOLT/Lesson_1.sql
@@ -0,0 +1,20 @@
+-- L1
+
+-- E1
+SELECT title FROM movies;
+
+-- E2
+
+SELECT director FROM movies;
+
+-- E3
+
+SELECT title, director FROM movies;
+
+-- E4
+
+SELECT title, year FROM movies;
+
+-- E5
+
+SELECT * FROM movies;
