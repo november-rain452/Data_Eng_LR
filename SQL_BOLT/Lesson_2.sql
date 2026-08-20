22decf377cf1a4d0fa4f7479a0453e7f10d155e8
Aditya Sinha <ranjanaditya452@gmail.com>
2026-08-20 23:53:26 +0530

    Lesson 2

---
+++ SQL_BOLT/Lesson_2.sql
@@ -0,0 +1,17 @@
+-- L2
+
+-- E1
+
+SELECT * FROM movies where id = 6;
+
+-- E2
+
+Select * from movies where year between 2000 and 2010;
+
+-- E3
+
+Select * from movies where year not between 2000 and 2010;
+ 
+-- E4
+
+Select * from movies limit 5;
