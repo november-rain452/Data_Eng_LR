f7dcd389f400c4efc7bff4eeab82e6aabe05bd41
Aditya Sinha <ranjanaditya452@gmail.com>
2026-08-20 23:57:59 +0530

    Lesson 3

---
+++ SQL_BOLT/Lesson_3.sql
@@ -0,0 +1,17 @@
+-- L3
+
+-- E1
+
+SELECT * FROM movies where title like "Toy Story%";
+
+-- E2
+
+SELECT * FROM movies where director = "John Lasseter";
+
+-- E3
+
+SELECT * FROM movies where director != "John Lasseter";
+
+-- E4
+
+SELECT * FROM movies where Title like "Wall-%"
