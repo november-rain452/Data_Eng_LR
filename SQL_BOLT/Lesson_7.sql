04739033fb2da7d7d9e5e2c6d006a51c859379b9
Aditya Sinha <ranjanaditya452@gmail.com>
2026-08-21 16:01:48 +0530

    Lesson 7

--- SQL_BOLT/Lesson_7.sql
+++ SQL_BOLT/Lesson_7.sql
@@ -7,3 +7,7 @@ SELECT distinct building FROM employees e left join buildings b on e.building = 
 -- E2
 
 SELECT * FROM buildings;
+
+-- E3
+
+SELECT distinct role, building_name FROM buildings b left join employees e on b.building_name = e.building;
