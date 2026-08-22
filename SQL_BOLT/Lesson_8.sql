1c55210f9f691b6f5e005c80c909435165bb4582
Aditya Sinha <ranjanaditya452@gmail.com>
2026-08-22 17:28:01 +0530

    Lesson 8

---
+++ SQL_BOLT/Lesson_8.sql
@@ -0,0 +1,9 @@
+-- L8
+
+-- E1
+
+SELECT * FROM employees e left join buildings b on b.building_name = e.building where e.building is null;
+
+-- E2
+
+SELECT * FROM buildings b left join employees e on b.building_name = e.building where e.building is null;
