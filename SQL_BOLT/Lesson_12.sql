707c9f561a244a3a8ae7d729675c39bce42b25b0
Aditya Sinha <ranjanaditya452@gmail.com>
2026-08-26 00:00:07 +0530

    Lesson 12

--- SQL_BOLT/Lesson_12.sql
+++ SQL_BOLT/Lesson_12.sql
@@ -1,11 +1,11 @@
 -- L12
 
--- L1
+-- E1
 
 SELECT director, count(title) 
 FROM movies m join boxoffice b on m.id = b.movie_id group by director;
 
--- L2
+-- E2
 
 SELECT director, sum(domestic_sales) + sum(international_sales) 
 FROM movies m join boxoffice b on m.id = b.movie_id group by director;
