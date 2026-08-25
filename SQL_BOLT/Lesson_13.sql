f24289361a77d0bf549653760f74c48196e6cd57
Aditya Sinha <ranjanaditya452@gmail.com>
2026-08-26 00:07:53 +0530

    Lesson 13

---
+++ SQL_BOLT/Lesson_13.sql
@@ -0,0 +1,10 @@
+-- L13
+
+-- E1
+
+insert into movies (title,director,year,length_minutes)
+values ("Toy Story 4", "John Lasseter", 1900, 81);
+
+-- E2
+
+insert into boxoffice values (15, 8.7, 340000000, 270000000);
