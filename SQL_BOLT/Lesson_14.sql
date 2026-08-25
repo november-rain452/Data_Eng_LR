b1315fd929005261384a01d0e9f7dec38ff5f480
Aditya Sinha <ranjanaditya452@gmail.com>
2026-08-26 01:50:51 +0530

    L14

--- SQL_BOLT/Lesson_14.sql
+++ SQL_BOLT/Lesson_14.sql
@@ -7,3 +7,8 @@ update movies set director= "John Lasseter" where  title = "A Bug's Life";
 -- E2
 
 update movies set year = "1999" where  title = "Toy Story 2";
+ 
+-- E3
+
+update movies set title = "Toy Story 3" , director = "Lee Unkrich" 
+where  title = "Toy Story 8";
