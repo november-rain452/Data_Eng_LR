2efa4725028c2a9313e4628f52c765044d2f578d
Aditya Sinha <ranjanaditya452@gmail.com>
2026-08-30 02:25:30 +0530

    Datalemur intermediate aggregates

--- DATA LEMUR/Intermediate/aggregates.sql
+++ DATA LEMUR/Intermediate/aggregates.sql
@@ -5,3 +5,14 @@ SELECT count(*) FROM pharmacy_sales;
 -- Sum
 SELECT count(product_id),sum(total_sales) FROM pharmacy_sales where manufacturer = 'Pfizer';
 
+-- Avg
+
+SELECT avg(open) FROM stock_prices where ticker = 'GOOG';
+
+-- Min 
+
+SELECT min(open) FROM stock_prices where ticker = 'MSFT';
+
+-- Max
+
+SELECT max(open) FROM stock_prices where ticker = 'NFLX';
