a220c32645a4de526ff8bdc6dc0e004709f7d529
Aditya Sinha <ranjanaditya452@gmail.com>
2026-09-03 23:52:06 +0530

    Arithmetic 2

--- DATA LEMUR/Intermediate/arithmetic.sql
+++ DATA LEMUR/Intermediate/arithmetic.sql
@@ -1,3 +1,8 @@
 -- E1
 
 SELECT card_name,(max(issued_amount)-min(issued_amount)) as difference FROM monthly_cards_issued group by card_name order by difference desc;
+
+-- E2
+
+SELECT drug, total_sales - cogs as profit FROM pharmacy_sales order by profit desc limit 3;
+
