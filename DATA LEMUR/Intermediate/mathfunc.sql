1d24a8945ad0f4d5681b52095f8340a446f32623
Aditya Sinha <ranjanaditya452@gmail.com>
2026-09-05 23:58:42 +0530

    math funcs

--- DATA LEMUR/Intermediate/mathfunc.sql
+++ DATA LEMUR/Intermediate/mathfunc.sql
@@ -1,4 +1,11 @@
 --E1
 
 SELECT drug,CEIL( total_sales/units_sold) as unit 
-FROM pharmacy_sales where manufacturer = 'Merck' order by unit ;
+FROM pharmacy_sales where manufacturer = 'Merck' 
+order by unit ;
+
+-- E2
+
+SELECT drug,abs( total_sales-units_sold) as unit 
+FROM pharmacy_sales where manufacturer = 'Apollo' 
+order by unit ;
