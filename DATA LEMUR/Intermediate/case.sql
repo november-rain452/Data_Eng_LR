8fed7dc06d59977c66c8e1b403aaff1666238b99
Aditya Sinha <ranjanaditya452@gmail.com>
2026-09-11 07:25:33 +0530

    conditionals/case

--- DATA LEMUR/Intermediate/case.sql
+++ DATA LEMUR/Intermediate/case.sql
@@ -8,3 +8,14 @@ when avg_likes >=15000 then 'Super Likes'
 End as cos_likes
 
 FROM marvel_avengers order by avg_likes desc;
+
+--E2
+
+SELECT 
+count(case 
+when device_type = 'tablet' or device_type = 'phone' then 1
+end) as mobile_views,
+count(case 
+when device_type = 'laptop' then 1
+end) as laptop_views
+FROM viewership;
